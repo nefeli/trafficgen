ef2c931aa58ff42fe176c429f7c13bbd0c4d4aa3
Melvin Walls <mwalls67@gmail.com>
2017-02-09 14:15:08 -0800

    make mac/ip addresses configurable for all modes

--- generator/common.py
+++ generator/common.py
@@ -30,13 +30,19 @@ class Pipeline(object):
 
 class TrafficSpec(object):
     def __init__(self, loss_rate=None, latency=False, pps=None, mbps=None,
-                 cores='0'):
+                 cores='0', src_mac='02:1e:67:9f:4d:bb',
+                 dst_mac='02:1e:67:9f:4d:bb', src_ip='192.168.0.1',
+                 dst_ip='10.0.0.1'):
         self.loss_rate = loss_rate
         self.latency = latency
         self.pps = pps
         self.mbps = None
         if latency:
             self.mbps = 100
+        self.src_mac = src_mac
+        self.dst_mac = dst_mac
+        self.src_ip = src_ip
+        self.dst_ip = dst_ip
         self.cores = list(map(int, cores.split(' ')))
 
 
@@ -49,28 +55,23 @@ class UdpSpec(TrafficSpec):
 
 
 class HttpSpec(TrafficSpec):
-    def __init__(self, num_flows=4000, src_mac='02:1e:67:9f:4d:aa',
-                 dst_mac='02:1e:67:9f:4d:bb', src_ip='192.168.0.1',
-                 dst_ip='10.0.0.1', src_port=1001, **kwargs):
+    def __init__(self, num_flows=4000, src_port=1001, **kwargs):
         self.num_flows = num_flows
-        self.src_mac = src_mac
-        self.dst_mac = dst_mac
-        self.src_ip = src_ip
-        self.dst_ip = dst_ip
         self.src_port = src_port
         super(HttpSpec, self).__init__(**kwargs)
 
 
 class FlowGenSpec(TrafficSpec):
-    def __init__(self, pkt_size=60, num_flows=10,
-                 flow_duration=5, flow_rate=None, arrival='uniform',
-                 duration='uniform', **kwargs):
+    def __init__(self, pkt_size=60, num_flows=10, flow_duration=5,
+                 flow_rate=None, arrival='uniform', duration='uniform',
+                 src_port=1001, **kwargs):
         self.pkt_size = pkt_size
         self.num_flows = num_flows
         self.flow_duration = flow_duration
         self.flow_rate = flow_rate
         self.arrival = arrival
         self.duration = duration
+        self.src_port = src_port
         super(FlowGenSpec, self).__init__(**kwargs)
 
 
