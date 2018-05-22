a75696ae4c7b73e2734f39df9e1ab01bde155e7b
Melvin Walls <mwalls67@gmail.com>
2018-05-21 21:58:35 -0700

    drop HTTP mode

--- generator/modes/__init__.py
+++ generator/modes/__init__.py
@@ -1,5 +1,4 @@
 from generator.modes.udp import UdpMode
 from generator.modes.flowgen import FlowGenMode
-from generator.modes.http import HttpMode
 from generator.modes.dot1q import Dot1QMode
 from generator.modes.dot1ad import Dot1ADMode
